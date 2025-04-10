53aa053dbff27022394158cbfd9d90558f5cd42a
douqin <dagxuanlam@gmail.com>
2025-04-10 19:34:38 +0700

    feature: add component fts

--- infras/db/run-scripts.sh
+++ infras/db/run-scripts.sh
@@ -10,16 +10,4 @@ echo "Executing script 1: Create database"
 # Wait a moment for database to be available
 sleep 3
 
-# Create tables
-echo "Executing script 2: Create tables"
-# /opt/mssql-tools/bin/sqlcmd -S db-trototvn -U sa -P "$1" -d TroTotVN -i /scripts/02-create-tables.sql -C
-
-# Create stored procedures
-echo "Executing script 3: Create stored procedures"
-/opt/mssql-tools/bin/sqlcmd -S db-trototvn -U sa -P "$1" -d TroTotVN -i /scripts/03-create-stored-procedures.sql -C
-
-# Seed data
-echo "Executing script 4: Seed data"
-/opt/mssql-tools/bin/sqlcmd -S db-trototvn -U sa -P "$1" -d TroTotVN -i /scripts/04-seed-data.sql -C
-
 echo "Database setup complete at: $(date)"
